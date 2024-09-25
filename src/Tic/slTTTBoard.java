ddc254c246a0ee5da2abbd7fe9d659e83527c15c
Sumo-1 <112205168+Sumo-1@users.noreply.github.com>
2024-09-24 19:53:40 -0700

    first part of code

--- src/Tic/slTTTBoard.java
+++ src/Tic/slTTTBoard.java
@@ -79,3 +79,19 @@ public class slTTTBoard {
         }
         return 0; // Game finished
     }
+
+    private void playerTurn(Scanner scanner) {
+        int row, col;
+        while (true) {
+            System.out.println("Player's turn. Enter row (1-3) and column (1-3): ");
+            row = scanner.nextInt() - 1;
+            col = scanner.nextInt() - 1;
+
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
+                board[row][col] = playerMarker;
+                break;
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+    }
