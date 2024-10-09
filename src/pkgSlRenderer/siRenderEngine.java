6533d2187b0c20fcdfc5983d2eca07d247a71a79
Sumo-1 <112205168+Sumo-1@users.noreply.github.com>
2024-10-08 21:34:47 -0700

    Merge remote-tracking branch 'assignment3/main'

--- src/pkgSlRenderer/siRenderEngine.java
+++ src/pkgSlRenderer/siRenderEngine.java
@@ -17,3 +17,23 @@ public class siRenderEngine {
                 // Clear the panel (set background to black)
                 setBackground(Color.BLACK
                 );
+
+
+
+                // Draw multiple circles at random postions
+                Random rand = new Random();
+                for (int i = 0; i < 100; i++) {// Draw 10 circles
+                    int x = rand.nextInt(getWidth() -50); // Random x postion
+                    int y = rand.nextInt(getHeight() -50); // Random y postion
+                    int diameter = 50; // Fixed diamter for each circle
+                    Color randomColor = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+                    g.setColor(randomColor); // set random color
+                    g.fillOval(x, y, diameter, diameter); // Draw the cricle
+                }
+            }
+        });
+
+        // Start the render loop (using Swing's repaint mechanism)
+        siWindowManager.get().startRenderLoop();
+    }
+}
